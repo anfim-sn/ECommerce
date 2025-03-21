116d3dac1c40f26bbd4f455b104412346be5be22
Anfim Snegirev <asnegirev@eqvantalab.ru>
2025-03-20 22:16:17 -0400

    add validation

--- src/ECommerce.OrderService/BusinessLogicLayer/Services/OrderService.cs
+++ src/ECommerce.OrderService/BusinessLogicLayer/Services/OrderService.cs
@@ -3,11 +3,19 @@ using BusinessLogicLayer.DTO;
 using BusinessLogicLayer.ServiceContracts;
 using DataAccessLayer.Entities;
 using DataAccessLayer.RepositoryContracts;
+using FluentValidation;
 using MongoDB.Driver;
 
 namespace BusinessLogicLayer.Services;
 
-public class OrderService(IOrderRepository orderRepository, IMapper mapper) : IOrderService
+public class OrderService(
+    IOrderRepository orderRepository, 
+    IMapper mapper, 
+    IValidator<OrderAddRequest> orderAddRequestValidator,
+    IValidator<OrderItemAddRequest> orderItemAddRequestValidator,
+    IValidator<OrderUpdateRequest> orderUpdateRequestValidator,
+    IValidator<OrderItemUpdateRequest> orderItemUpdateRequestValidator
+    ) : IOrderService
 {
 
     public async Task<IEnumerable<OrderResponse>> GetOrders()
@@ -28,17 +36,74 @@ public class OrderService(IOrderRepository orderRepository, IMapper mapper) : IO
         return mapper.Map<OrderResponse>(order);
     }
     
-    public async Task<OrderResponse?> AddOrder(OrderAddRequest order)
+    public async Task<OrderResponse?> AddOrder(OrderAddRequest orderAddRequest)
     {
-        var orderEntity = mapper.Map<Order>(order);
+        if (orderAddRequest == null)
+            throw new ArgumentNullException(nameof(orderAddRequest));
+        
+        var validationResult = await orderAddRequestValidator.ValidateAsync(orderAddRequest);
+        if (!validationResult.IsValid)
+        {
+            var errors = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
+            throw new ArgumentException(errors);
+        }
+        
+        var orderItemsValidation = orderAddRequest.OrderItems.Select(orderItemAddRequestValidator.Validate);
+        if (orderItemsValidation.Any(x => !x.IsValid))
+        {
+            var errors = string.Join(", ", orderItemsValidation.SelectMany(x => x.Errors).Select(x => x.ErrorMessage));
+            throw new ArgumentException(errors);
+        }
+        
+        //Check UserId in Users microservice here
+        
+        var orderEntity = mapper.Map<Order>(orderAddRequest);
+        
+        foreach(var orderItem in orderEntity.OrderItems)
+            orderItem.TotalPrice = orderItem.Quantity * orderItem.UnitPrice;
+
+        orderEntity.TotalBill = orderEntity.OrderItems.Sum(x => x.TotalPrice);
+        
         var result = await orderRepository.AddOrder(orderEntity);
+        
+        if (result == null)
+            return null;
+        
         return mapper.Map<OrderResponse>(result);
     }
     
-    public async Task<OrderResponse?> UpdateOrder(OrderUpdateRequest order)
+    public async Task<OrderResponse?> UpdateOrder(OrderUpdateRequest orderUpdateRequest)
     {
-        var orderEntity = mapper.Map<Order>(order);
+        if (orderUpdateRequest == null)
+            throw new ArgumentNullException(nameof(orderUpdateRequest));
+
+        var validationResult = await orderUpdateRequestValidator.ValidateAsync(orderUpdateRequest);
+        if (!validationResult.IsValid)
+        {
+            var errors = string.Join(", ", validationResult.Errors.Select(x => x.ErrorMessage));
+            throw new ArgumentException(errors);
+        }
+
+        var orderItemsValidation = orderUpdateRequest.OrderItems.Select(orderItemUpdateRequestValidator.Validate);
+        if (orderItemsValidation.Any(x => !x.IsValid))
+        {
+            var errors = string.Join(", ", orderItemsValidation.SelectMany(x => x.Errors).Select(x => x.ErrorMessage));
+            throw new ArgumentException(errors);
+        }
+
+        //Check UserId in Users microservice here
+        var orderEntity = mapper.Map<Order>(orderUpdateRequest);
+
+        foreach (var orderItem in orderEntity.OrderItems)
+            orderItem.TotalPrice = orderItem.Quantity * orderItem.UnitPrice;
+
+        orderEntity.TotalBill = orderEntity.OrderItems.Sum(x => x.TotalPrice);
+        
         var result = await orderRepository.UpdateOrder(orderEntity);
+
+        if (result == null)
+            return null;
+        
         return mapper.Map<OrderResponse>(result);
     }
     
