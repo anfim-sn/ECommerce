e051d4d442665da9e2e2e201e2d911884208a745
Anfim Snegirev <asnegirev@eqvantalab.ru>
2025-04-01 18:40:35 -0400

    fix

--- src/ECommerce.UserService/ECommerce.Infrastructure/Repositories/UsersRepository.cs
+++ src/ECommerce.UserService/ECommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -12,7 +12,7 @@ internal class UsersRepository(DapperDbContext DbContext) : IUserRepository
     {
         user.UserId = Guid.NewGuid();
 
-        const string query = "INSERT INTO \"public\".\"Users\" (\"UserId\", \"Email\", \"PersonName\", \"Gender\", \"Password\") " +
+        const string query = "INSERT INTO \"public\".\"users\" (\"UserId\", \"Email\", \"PersonName\", \"Gender\", \"Password\") " +
                              "VALUES (@UserId, @Email, @PersonName, @Gender, @Password);";
 
         var rowAffected = await DbContext.DbConnection.ExecuteAsync(query, user);
@@ -22,7 +22,7 @@ internal class UsersRepository(DapperDbContext DbContext) : IUserRepository
     
     public async Task<ApplicationUser?> GetUserByEmailAndPassword(string email, string password)
     {
-        const string query = $"SELECT * FROM \"public\".\"Users\" WHERE \"Email\" = @Email AND \"Password\" = @Password;";
+        const string query = $"SELECT * FROM \"public\".\"users\" WHERE \"Email\" = @Email AND \"Password\" = @Password;";
         var param = new { Email = email, Password = password };
 
         var result = await DbContext.DbConnection.QueryFirstOrDefaultAsync<ApplicationUser>(query, param);
@@ -31,7 +31,7 @@ internal class UsersRepository(DapperDbContext DbContext) : IUserRepository
     }
     public Task<ApplicationUser?> GetUserByUserId(Guid userId)
     {
-        const string query = $"SELECT * FROM \"public\".\"Users\" WHERE \"UserId\" = @UserId;";
+        const string query = $"SELECT * FROM \"public\".\"users\" WHERE \"UserId\" = @UserId;";
         var param = new { UserId = userId };
         
         return DbContext.DbConnection.QueryFirstOrDefaultAsync<ApplicationUser>(query, param);
